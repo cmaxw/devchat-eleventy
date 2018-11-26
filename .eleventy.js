e7df0f0ad1aea3e4c3b34fc2aa713106d3cb4508
Charles Max Wood <chuck@devchat.tv>
2018-11-26 14:55:36 -0700

    Move .eleventy.js to see if that makes the magic happen.

--- .eleventy.js
+++ .eleventy.js
@@ -26,6 +26,17 @@ module.exports = function(eleventyConfig) {
           "njk",
           "html",
           "liquid"
-        ]
+        ],
+
+        markdownTemplateEngine: "liquid",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        passthroughFileCopy: true,
+        dir: {
+          input: "src",
+          includes: "_includes",
+          data: "_data",
+          output: "_site"
+        }
     };
 };
