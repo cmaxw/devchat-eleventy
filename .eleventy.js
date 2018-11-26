1c1f17cde8fed7b649675a14617e872f6a8f8409
Charles Max Wood <chuck@devchat.tv>
2018-11-26 07:50:35 -0700

    Fiddling with looping on the front page PLUS adding breaking post content for recent episodes.

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,22 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("js");
     eleventyConfig.addPassthroughCopy("fonts");
 
+    eleventyConfig.addFilter("head", (array, n) => {
+        if( n < 0 ) {
+          return array.slice(n);
+        }
+    
+        return array.slice(0, n);
+    });
+    
+    eleventyConfig.addFilter("range", (array, m, n) => {
+        if( n < 0 ) {
+          return array.slice(n);
+        }
+    
+        return array.slice(m, n);
+    });
+    
     return {
         templateFormats: [
           "md",
