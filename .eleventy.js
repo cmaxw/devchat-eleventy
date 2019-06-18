19f50d732f9c529fbc8e560ae8d3b8f1e3c5c88d
Charles Max Wood <chuck@devchat.tv>
2019-06-17 23:34:42 -0600

    file pass through admin

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('js');
   eleventyConfig.addPassthroughCopy('fonts');
+  eleventyConfig.addPassthroughCopy('admin');
 
   eleventyConfig.addNunjucksFilter('date', nunjucksDate);
 
