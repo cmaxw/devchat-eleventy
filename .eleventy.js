a06f6934ec05e94f39647ed8fdefbd2ab5734bba
Charles Max Wood <chuck@devchat.tv>
2019-12-27 23:35:36 -0700

    Remove netlify CMS

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('js');
   eleventyConfig.addPassthroughCopy('fonts');
-  eleventyConfig.addPassthroughCopy('admin');
   eleventyConfig.addPassthroughCopy('_redirects');
 
   eleventyConfig.addNunjucksFilter('date', nunjucksDate);
